45fdd86691f9eb104fbf2468b77cd63b98c69033
Anek <64838666+AnekAnil@users.noreply.github.com>
2022-01-27 16:33:02 +0530

    Add files via upload

---
+++ BOT Source Code/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\medicalbot-91e7-bot-abc4-Web-Deploy.pubxml -p:Password=JFjeLYwJs82w4jTNNmeD0XiELF22XRMluQtn9BbLJ7ePEPshFHhurXqkKJ3E
+
